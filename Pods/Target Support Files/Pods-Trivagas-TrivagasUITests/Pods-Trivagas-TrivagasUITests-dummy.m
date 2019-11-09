660de5d589c918151b8a2e4ea1d0fad4b93a68d1
Clinton Barreto <clinton@trilogo.com.br>
2019-11-09 10:23:43 -0300

    Configuração básica do Cocoa Pods

---
+++ Pods/Target Support Files/Pods-Trivagas-TrivagasUITests/Pods-Trivagas-TrivagasUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Trivagas_TrivagasUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Trivagas_TrivagasUITests
+@end
---
+++ Pods/Target Support Files/Pods-Trivagas/Pods-Trivagas-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Trivagas : NSObject
+@end
+@implementation PodsDummy_Pods_Trivagas
+@end
