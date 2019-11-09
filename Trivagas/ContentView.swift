084c9dff988f2367a33fc1598389460f3e192498
Clinton Barreto <clinton@trilogo.com.br>
2019-11-09 10:03:32 -0300

    Initial Commit

---
+++ Trivagas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trivagas
+//
+//  Created by Trilogo Dev1 on 09/11/19.
+//  Copyright © 2019 Trilogo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
